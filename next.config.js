d67d2cb807e52578101fabf42529fa80baf4ffc5
Keshav Sharma <thesharmakeshav@gmail.com>
2026-07-17 20:29:45 +0530

    Revert output export for Render Web Service compatibility

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   reactStrictMode: false,
   images: {
-    unoptimized: true,
     remotePatterns: [
       { protocol: 'https', hostname: '**' },
     ],
